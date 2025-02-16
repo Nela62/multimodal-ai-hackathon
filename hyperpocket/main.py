1772a55f8b35282617212cf70752f46978047185
Kirill Igumenshchev <kirill.igum@gmail.com>
2025-02-15 16:02:27 -0800

    feat: Add hyperpocket main script and settings file

---
+++ hyperpocket/main.py
@@ -0,0 +1,43 @@
+import os
+
+from llama_index.core.agent import AgentRunner, FunctionCallingAgent
+from llama_index.core.memory import ChatMemoryBuffer
+from llama_index.llms.openai import OpenAI
+
+from hyperpocket_llamaindex import PocketLlamaindex
+
+
+def build():
+    llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    pocket = PocketLlamaindex(
+        tools=[
+            "https://github.com/vessl-ai/hyperpocket/tree/main/tools/none/simple-echo-tool"
+        ]
+    )
+    tools = pocket.get_tools()
+
+    memory = ChatMemoryBuffer.from_defaults(chat_history=[], llm=llm)
+
+    agent = FunctionCallingAgent.from_tools(
+        tools=tools, llm=llm, memory=memory, verbose=True
+    )
+
+    return agent
+
+
+def run(agent: AgentRunner):
+    while True:
+        print("user(q to quit) : ", end="")
+        user_input = input()
+        if user_input == "q":
+            print("bye")
+            break
+        elif user_input == "":
+            continue
+
+        agent.chat(user_input)
+
+
+if __name__ == "__main__":
+    agent = build()
+    run(agent)
